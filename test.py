0091bf981f141d52040cfcae3f5a5ba205b8773c
ad18181 <amogh.dixit18@gmail.com>
2024-07-05 13:09:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-secret = 'password123!'
-
-password = 'thisisnotapassword' #nohorus
-
-command = 'print "this command is not secure!!"'
-
-exec(command)
-
-print(secret)
-
-assert 2 + 2 == 5, "Wrong!"
+print("hello")
